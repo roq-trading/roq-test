f3a22fcdbb5e3e20a5acc50cbd652e18454dc9a4
thraneh <thraneh@gmail.com>
2020-01-08 18:50:22 +0000

    make it compile

---
+++ src/roq/test/coinbase-pro.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CWD="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+if [ "$1" == "debug" ]; then
+	PREFIX="libtool --mode=execute gdb --args"
+else
+	PREFIX=
+fi
+
+NAME="trader"
+
+$PREFIX ./roq-test \
+	--name "$NAME" \
+	--exchange "coinbase-pro" \
+	--symbol "BTC-USD" \
+	$@
